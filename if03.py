b96ab58e936f50d7a15f9802e4e5b91593907b44
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-19 15:30:45 +0500

    if03.py

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and a<c or a>c and a<b:
+        return a
+    if b>a and b<c or b>c and b<a:
+        return b
+    if c>a and c<b or a>b and a<a:
+        return c
+
+print(main(23,34,28))
