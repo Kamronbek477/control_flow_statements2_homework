8c61ed15207196f7ed9fc1694ff3d5d6688da70f
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-20 10:45:48 +0500

    if06.py

--- if06.py
+++ if06.py
@@ -6,4 +6,29 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n%10
+    n//=10
+
+    b=n%10
+    n//=10
+
+    c=n%10
+    n//=10
+
+    d=n%10
+    n//=10
+
+    q=n%10
+    n//=10
+
+    if a>b and a>c and a>d and a>q:
+        return  1
+    if b>a and b>c and b>d and b>q:
+        return  2
+    if c>a and c>b and c>d and c>q :
+        return  3
+    if d>a and d>b and d>c and d>q :
+        return  4
+    if q>a and q>b and q>c and q>d:
+        return  5
+print(main(71945))
